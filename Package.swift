5370e9618246dad0760807ed7f7b67212c2cd667
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-11-21 13:36:52 +0500

    Bump swift-tools-version to 5.5

--- Package.swift
+++ Package.swift
@@ -1,20 +1,23 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
     name: "tuproq",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8)
+    ],
     products: [
         .library(name: "Tuproq", targets: ["Tuproq"])
     ],
     targets: [
         .target(name: "Tuproq"),
-        .testTarget(
-            name: "TuproqTests",
-            dependencies: [
-                .target(name: "Tuproq")
-            ]
-        )
+        .testTarget(name: "TuproqTests", dependencies: [
+            .target(name: "Tuproq")
+        ])
     ],
     swiftLanguageVersions: [.v5]
 )
