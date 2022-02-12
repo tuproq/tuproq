bceba4d297d316007896905fc374c5a2930eb8f2
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-02-12 16:05:11 +0500

    Create InsertIntoSQLExpression

---
+++ Sources/Tuproq/Expression/SQL/InsertIntoSQLExpression.swift
@@ -0,0 +1,33 @@
+final class InsertIntoSQLExpression: SQLExpression {
+    let table: String
+    let columns: [String]
+    let values: [Any?]
+
+    init(table: String, columns: [String] = .init(), values: [Any?]) {
+        self.table = table
+        self.columns = columns
+        self.values = values
+        var raw = "\(Kind.insertInto) \(table)"
+
+        if !columns.isEmpty {
+            raw += " (\(columns.map({ $0.description }).joined(separator: ", ")))"
+        }
+
+        if !values.isEmpty {
+            let values = values.map { value in
+                if let value = value {
+                    if let string = value as? String {
+                        return "\"\(string)\""
+                    }
+
+                    return "\(value)"
+                }
+
+                return "NULL"
+            }.joined(separator: ", ")
+            raw += " \(Kind.values) (\(values))"
+        }
+
+        super.init(raw: raw)
+    }
+}
---
+++ Tests/TuproqTests/Expression/SQL/InsertIntoSQLExpressionTests.swift
@@ -0,0 +1,29 @@
+@testable import Tuproq
+import XCTest
+
+final class InsertIntoSQLExpressionTests: XCTestCase {
+    func testInit() {
+        // Arrange
+        let table = "table"
+        let columns = ["column1", "column2", "column3"]
+        let values: [Any?] = [1, "value2", nil]
+
+        // Act
+        let expression = InsertIntoSQLExpression(table: table, columns: columns, values: values)
+
+        // Assert
+        XCTAssertEqual(expression.table, table)
+        XCTAssertEqual(expression.columns, columns)
+        XCTAssertEqual(expression.values.count, values.count)
+        XCTAssertEqual(expression.values.first as! Int, values.first as! Int)
+        XCTAssertEqual(expression.values[1] as! String, values[1] as! String)
+        XCTAssertNil(expression.values.last!)
+        XCTAssertEqual(
+            expression.raw,
+            """
+            INSERT INTO table (column1, column2, column3) VALUES (1, "value2", NULL)
+            """
+        )
+        XCTAssertEqual(expression.raw, expression.description)
+    }
+}
