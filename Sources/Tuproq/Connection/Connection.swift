7c2277ac1351a56656b2334140c98683936c0f97
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-02-12 16:30:48 +0500

    Refactor

--- Sources/Tuproq/Connection/Connection.swift
+++ Sources/Tuproq/Connection/Connection.swift
@@ -2,9 +2,9 @@ public class Connection {
     public static let defaultName = "default"
     public let name: String
     public let option: Option
-    public let entities: [Entity]
+    public let entities: [Any.Type]
 
-    public init(name: String = Connection.defaultName, option: Option, entities: [Entity] = .init()) {
+    public init(name: String = Connection.defaultName, option: Option, entities: [Any.Type] = .init()) {
         self.name = name
         self.option = option
         self.entities = entities
