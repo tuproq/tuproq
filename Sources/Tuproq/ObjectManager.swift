c4a483ed7766b1f0a3ac940ec08210912680ba0e
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-02-19 12:04:35 +0500

    Refactor ObjectManager

--- Sources/Tuproq/ObjectManager.swift
+++ Sources/Tuproq/ObjectManager.swift
@@ -1,11 +1,11 @@
 public protocol ObjectManager {
     func detach<E: Entity>(_ entity: E)
     func find<E: Entity, I: Hashable>(_ entityType: E.Type, id: I) async throws -> E?
-    func flush<E: Entity>(_ entity: E) async throws
     func flush() async throws
+    func flush<E: Entity>(_ entity: E) async throws
     func getRepository<R: Repository>(_ repositoryType: R.Type) -> R
     func merge<E: Entity>(_ entity: E)
     func persist<E: Entity>(_ entity: E) throws
-    func refresh<E: Entity>(_ entity: E)
+    func refresh<E: Entity>(_ entity: E) throws
     func remove<E: Entity>(_ entity: E)
 }
