e0f5739018bbf2850d32db7c2138967e6943bca7
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-03-11 16:02:16 +0500

    Create TuproqError

--- Sources/Tuproq/Error.swift
+++ Sources/Tuproq/Error.swift
@@ -1,3 +1,24 @@
+import Foundation
+
+public struct TuproqError: LocalizedError {
+    let message: String
+    public var errorDescription: String? { message }
+
+    init(_ errorType: ErrorType) {
+        self.init(errorType.message)
+    }
+
+    init(_ message: String? = nil) {
+        let errorType = String(describing: type(of: self))
+
+        if let message = message, !message.isEmpty {
+            self.message = "\(errorType): \(message)"
+        } else {
+            self.message = "\(errorType): \(ErrorType.unknown)"
+        }
+    }
+}
+
 enum ErrorType: CustomStringConvertible {
     case detachedObjectNotPersistable
     case entityToDictionaryFailed
