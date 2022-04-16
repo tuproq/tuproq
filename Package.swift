0d5c00173627424b823ed2d2c6ca8325f1ba4b6c
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-17 00:07:36 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "tuproq",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v12),
-        .tvOS(.v13),
-        .watchOS(.v6)
-    ],
     products: [
         .library(name: "Tuproq", targets: ["Tuproq"])
     ],
