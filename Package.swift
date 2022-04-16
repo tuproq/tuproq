f002d91c845a0051c394bb5c207d50ff1e899528
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-17 00:42:21 +0500

    Set the minimum platform versions

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "tuproq",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(name: "Tuproq", targets: ["Tuproq"])
     ],
