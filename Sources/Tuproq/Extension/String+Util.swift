4f173f417e40af76f6d65c6d02bf8382d5c8175a
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-08-10 22:19:16 +0500

    Add snakeCased variable extension

--- Sources/Tuproq/Extension/String+Util.swift
+++ Sources/Tuproq/Extension/String+Util.swift
@@ -1,5 +1,24 @@
+import Foundation
+
 extension String {
     var droppingLeadingSlash: String {
         first == "/" ? String(dropFirst()) : self
+    }
+
+    var snakeCased: String {
+        let acronymPattern = "([A-Z]+)([A-Z][a-z]|[0-9])"
+        let fullWordsPattern = "([a-z])([A-Z]|[0-9])"
+        let digitsFirstPattern = "([0-9])([A-Z])"
+
+        return snakeCase(with: acronymPattern)?
+            .snakeCase(with: fullWordsPattern)?
+            .snakeCase(with:digitsFirstPattern)?.lowercased() ?? lowercased()
+    }
+
+    private func snakeCase(with pattern: String) -> String? {
+        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        let range = NSRange(location: 0, length: count)
+
+        return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "$1_$2")
     }
 }
