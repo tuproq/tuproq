ebcb24a555b36d4522b2ed2b601af33c7254f7aa
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-08-21 12:52:58 +0500

    Create EntityMapping

---
+++ Sources/Tuproq/Mapping/EntityMapping.swift
@@ -0,0 +1,19 @@
+public protocol EntityMapping {
+    associatedtype E: Entity
+
+    var strategy: MappingStrategy { get }
+    var entity: E.Type { get }
+    var table: String { get }
+    var fields: Set<FieldMapping> { get }
+}
+
+extension EntityMapping {
+    var strategy: MappingStrategy { .same }
+
+    var table: String {
+        switch strategy {
+        case .same: return String(describing: entity).lowercased()
+        case .snakeCased: return String(describing: entity).lowercased()
+        }
+    }
+}
