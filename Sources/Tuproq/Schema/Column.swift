0f4cf9d2ced7c80a01ba4510532edcfd4fb410fe
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-08-23 21:12:04 +0500

    Nest Column inside Table

--- Sources/Tuproq/Schema/Column.swift
+++ Sources/Tuproq/Schema/Column.swift
@@ -1,5 +1,7 @@
-struct Column {
-    var name: String
-    var type: String
-    var constraints: [Constraint]
+extension Table {
+    struct Column {
+        var name: String
+        var type: String
+        var constraints: [Constraint]
+    }
 }
