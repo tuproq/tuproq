b2f073eaa83dc1c7518c355c13fb34d1a07fdc00
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-09-15 10:43:44 +0500

    Add isUnique to ParentMapping

--- Sources/Tuproq/Mapping/ParentMapping.swift
+++ Sources/Tuproq/Mapping/ParentMapping.swift
@@ -2,12 +2,20 @@ public struct ParentMapping: Hashable {
     public let name: String
     let parent: AnyEntityMapping
     public let column: String
+    public let isUnique: Bool
     public let isNullable: Bool
 
-    public init<M: EntityMapping>(name: String, parent: M, column: String? = nil, isNullable: Bool = false) {
+    public init<M: EntityMapping>(
+        name: String,
+        parent: M,
+        column: String? = nil,
+        isUnique: Bool = false,
+        isNullable: Bool = false
+    ) {
         self.name = name
         self.parent = AnyEntityMapping(parent)
         self.column = column ?? name
+        self.isUnique = isUnique
         self.isNullable = isNullable
     }
 
