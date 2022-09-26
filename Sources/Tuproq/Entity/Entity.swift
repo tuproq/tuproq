370d8d973417124939625d057103817395ff93af
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-09-26 12:43:27 +0500

    Conform Entity to Hashable

--- Sources/Tuproq/Entity/Entity.swift
+++ Sources/Tuproq/Entity/Entity.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public protocol Entity: AnyObject, Codable, Equatable {
+public protocol Entity: AnyObject, Codable, Hashable {
     associatedtype Identifiable: Codable, Hashable
     var id: Identifiable { get }
 
@@ -12,5 +12,13 @@ public extension Entity {
 
     static func == (lhs: Self, rhs: Self) -> Bool {
         ObjectIdentifier(lhs) == ObjectIdentifier(rhs) || lhs.id == rhs.id
+    }
+
+    func hash(into hasher: inout Hasher) {
+        if id as AnyObject is NSNull {
+            hasher.combine(ObjectIdentifier(self))
+        } else {
+            hasher.combine(id)
+        }
     }
 }
