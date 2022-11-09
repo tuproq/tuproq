ff4309bb29561c781595f30cf4fef864cc3a8987
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-11-09 14:04:21 +0500

    Refactor

--- Sources/Tuproq/Extension/String+Util.swift
+++ Sources/Tuproq/Extension/String+Util.swift
@@ -8,14 +8,6 @@ public extension String {
 }
 
 extension String {
-    var droppingLeadingSlash: String {
-        first == "/" ? String(dropFirst()) : self
-    }
-
-    var droppingLeadingUnderscore: String {
-        first == "_" ? String(dropFirst()) : self
-    }
-
     func snakeCase(_ letterCase: LetterCase = .lower) -> String {
         let acronymPattern = "([A-Z]+)([A-Z][a-z]|[0-9])"
         let fullWordsPattern = "([a-z])([A-Z]|[0-9])"
@@ -31,9 +23,9 @@ extension String {
     }
 
     private func snakeCase(with pattern: String) -> String? {
-        let regex = try? NSRegularExpression(pattern: pattern, options: [])
+        let regex = try? NSRegularExpression(pattern: pattern)
         let range = NSRange(location: 0, length: count)
 
-        return regex?.stringByReplacingMatches(in: self, options: [], range: range, withTemplate: "$1_$2")
+        return regex?.stringByReplacingMatches(in: self, range: range, withTemplate: "$1_$2")
     }
 }
