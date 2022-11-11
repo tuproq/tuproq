c1f135f578b191063711a01b65cc97f51d733e7b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-11-11 10:14:13 +0500

    Add camelCase String extension

--- Sources/Tuproq/Extension/String+Util.swift
+++ Sources/Tuproq/Extension/String+Util.swift
@@ -29,3 +29,17 @@ extension String {
         return regex?.stringByReplacingMatches(in: self, range: range, withTemplate: "$1_$2")
     }
 }
+
+extension String {
+    var lowercasingFirst: String { prefix(1).lowercased() + dropFirst() }
+    var uppercasingFirst: String { prefix(1).uppercased() + dropFirst() }
+
+    var camelCased: String {
+        guard !isEmpty else { return "" }
+        let parts = components(separatedBy: .alphanumerics.inverted)
+        let first = parts.first!.lowercasingFirst
+        let rest = parts.dropFirst().map { $0.uppercasingFirst }
+
+        return ([first] + rest).joined()
+    }
+}
