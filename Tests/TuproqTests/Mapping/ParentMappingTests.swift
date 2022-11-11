b71d29f6fec9483821adb96ad64a425bd2272bde
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-11-11 12:34:16 +0500

    Update tests for ParentMapping

--- Tests/TuproqTests/Mapping/ParentMappingTests.swift
+++ Tests/TuproqTests/Mapping/ParentMappingTests.swift
@@ -10,6 +10,7 @@ final class ParentMappingTests: XCTestCase {
         // Arrange
         let field = "author"
         let entity: any Entity.Type = Author.self
+        let inversedBy = "posts"
         let isUnique = true
         let isNullable = false
         let column = JoinTable.Column(name: "author_id", isUnique: isUnique, isNullable: isNullable)
@@ -20,14 +21,16 @@ final class ParentMappingTests: XCTestCase {
         // Assert
         XCTAssertEqual(mapping.field, field)
         XCTAssertTrue(mapping.entity == entity)
+        XCTAssertNil(mapping.inversedBy)
         XCTAssertEqual(mapping.column, .init(stringLiteral: Configuration.namingStrategy.joinColumn(field: field)))
 
         // Act
-        mapping = ParentMapping(field: field, entity: entity, column: column)
+        mapping = ParentMapping(field: field, entity: entity, inversedBy: inversedBy, column: column)
 
         // Assert
         XCTAssertEqual(mapping.field, field)
         XCTAssertTrue(mapping.entity == entity)
+        XCTAssertEqual(mapping.inversedBy, inversedBy)
         XCTAssertEqual(mapping.column, column)
     }
 }
