872faef98f6d442349c1923553187c9a39b4f703
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-11-11 10:16:20 +0500

    Simplify naming mappings with typealias

--- Sources/Tuproq/Mapping/EntityMapping.swift
+++ Sources/Tuproq/Mapping/EntityMapping.swift
@@ -1,21 +1,27 @@
 public protocol EntityMapping {
     associatedtype E: Entity
 
+    typealias ID = IDMapping
+    typealias Field = FieldMapping
+    typealias Parent = ParentMapping
+    typealias Child = ChildMapping
+    typealias Sibling = SiblingMapping
+
     var entity: E.Type { get }
     var table: String { get }
-    var ids: Set<IDMapping> { get }
-    var fields: Set<FieldMapping> { get }
-    var parents: Set<ParentMapping> { get }
-    var children: Set<ChildMapping> { get }
-    var siblings: Set<SiblingMapping> { get }
+    var ids: Set<ID> { get }
+    var fields: Set<Field> { get }
+    var parents: Set<Parent> { get }
+    var children: Set<Child> { get }
+    var siblings: Set<Sibling> { get }
 }
 
 public extension EntityMapping {
     var entity: E.Type { E.self }
     var table: String { Configuration.namingStrategy.table(entity: entity) }
-    var ids: Set<IDMapping> { [.init()] }
-    var fields: Set<FieldMapping> { .init() }
-    var parents: Set<ParentMapping> { .init() }
-    var children: Set<ChildMapping> { .init() }
-    var siblings: Set<SiblingMapping> { .init() }
+    var ids: Set<ID> { [.init()] }
+    var fields: Set<Field> { .init() }
+    var parents: Set<Parent> { .init() }
+    var children: Set<Child> { .init() }
+    var siblings: Set<Sibling> { .init() }
 }
