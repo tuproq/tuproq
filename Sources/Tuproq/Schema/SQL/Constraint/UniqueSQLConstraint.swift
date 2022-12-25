4e0197416c4dad89abd4d58dddfe5bc6e085a8b6
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-12-25 14:43:46 +0500

    Add init options to UniqueSQLConstraint

--- Sources/Tuproq/Schema/SQL/Constraint/UniqueSQLConstraint.swift
+++ Sources/Tuproq/Schema/SQL/Constraint/UniqueSQLConstraint.swift
@@ -1,4 +1,25 @@
-struct UniqueSQLConstraint: SQLConstraint {
+struct UniqueSQLConstraint: SQLConstraint, ExpressibleByArrayLiteral, ExpressibleByStringLiteral {
     let name = "UNIQUE"
-    var column: String
+    let columns: Set<String>
+    let index: String?
+
+    init(column: String, index: String? = nil) {
+        columns = [column]
+        self.index = index
+    }
+
+    init(stringLiteral column: String) {
+        columns = [column]
+        index = nil
+    }
+
+    init(columns: Set<String>, index: String? = nil) {
+        self.columns = columns
+        self.index = index
+    }
+
+    init(arrayLiteral columns: String...) {
+        self.columns = .init(columns)
+        index = nil
+    }
 }
