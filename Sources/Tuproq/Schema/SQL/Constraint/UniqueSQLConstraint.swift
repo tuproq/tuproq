da6f9829689222385a1c3431b77ab9080f6c91b9
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-12-25 14:57:04 +0500

    Refactor UniqueSQLConstraint

--- Sources/Tuproq/Schema/SQL/Constraint/UniqueSQLConstraint.swift
+++ Sources/Tuproq/Schema/SQL/Constraint/UniqueSQLConstraint.swift
@@ -1,25 +1,22 @@
 struct UniqueSQLConstraint: SQLConstraint, ExpressibleByArrayLiteral, ExpressibleByStringLiteral {
     let name = "UNIQUE"
     let columns: Set<String>
-    let index: String?
-
-    init(column: String, index: String? = nil) {
-        columns = [column]
-        self.index = index
-    }
+    let index: String
 
     init(stringLiteral column: String) {
-        columns = [column]
-        index = nil
+        self.init(columns: [column])
+    }
+
+    init(arrayLiteral columns: String...) {
+        self.init(columns: .init(columns))
+    }
+
+    init(column: String, index: String? = nil) {
+        self.init(columns: [column], index: index)
     }
 
     init(columns: Set<String>, index: String? = nil) {
         self.columns = columns
-        self.index = index
-    }
-
-    init(arrayLiteral columns: String...) {
-        self.columns = .init(columns)
-        index = nil
+        self.index = index ?? "\(columns.joined(separator: "_"))_idx"
     }
 }
