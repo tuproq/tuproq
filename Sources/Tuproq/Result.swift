31a594c5340203c451fbf975a6e1d3d33233e118
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-11 15:08:50 +0500

    Make Result class's init public

--- Sources/Tuproq/Result.swift
+++ Sources/Tuproq/Result.swift
@@ -2,7 +2,7 @@ public final class Result {
     public let columns: [String]
     public let rows: [[Any?]]
 
-    init(columns: [String], rows: [[Any?]]) {
+    public init(columns: [String], rows: [[Any?]]) {
         self.columns = columns
         self.rows = rows
     }
