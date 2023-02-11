e0da63d18a5e45f63f925d7063c6b13a9de443cf
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-02-11 15:08:12 +0500

    Rename connect() method to open()

--- Sources/Tuproq/Connection.swift
+++ Sources/Tuproq/Connection.swift
@@ -2,7 +2,7 @@ public protocol Connection {
     var driver: DatabaseDriver { get }
 
     @discardableResult
-    func connect() async throws -> Self
+    func open() async throws -> Self
     func close() async throws
     func query(_ string: String) async throws -> [Result]
 }
