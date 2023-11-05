a50534ed9244035c1316ebad7ddf194bdee0c2a1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2023-11-05 11:15:41 +0500

    Make inits public

--- Sources/Tuproq/Connection.swift
+++ Sources/Tuproq/Connection.swift
@@ -26,13 +26,18 @@ public struct Column: CustomStringConvertible, Hashable {
     public let name: String
     public let tableID: Int32 // TODO: a temporary solution (PostgreSQL specific)
     public var description: String { name }
+
+    public init(name: String, tableID: Int32) {
+        self.name = name
+        self.tableID = tableID
+    }
 }
 
 public final class QueryResult {
     public let columns: [Column]
     public internal(set) var rows = [[Codable?]]()
 
-    init(columns: [Column], rows: [[Codable?]]) {
+    public init(columns: [Column], rows: [[Codable?]]) {
         self.columns = columns
         self.rows = rows
     }
