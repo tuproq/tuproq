a7cae6d2c02a0595eb001d8acfb4fc1065258d87
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2024-07-30 06:38:52 +0500

    Fix type conversion for insertion and updating

--- Sources/Tuproq/Expression/SQL/InsertIntoSQLExpression.swift
+++ Sources/Tuproq/Expression/SQL/InsertIntoSQLExpression.swift
@@ -17,14 +17,52 @@ final class InsertIntoSQLExpression: SQLExpression {
 
         if !values.isEmpty {
             let values = values.map { value in
-                if let value = value {
-                    if let string = value as? String {
+                if let value {
+                    if value as AnyObject is NSNull {
+                        return "NULL"
+                    } else if let bool = value as? Bool {
+                        return "\(bool)"
+                    } else if let character = value as? Character {
+                        return "'\(character)'"
+                    } else if let data = value as? Data {
+                        return "'\(String(data: data, encoding: .utf8) ?? "")'"
+                    } else if let date = value as? Date {
+                        return "'\(date)'"
+                    } else if let double = value as? Double {
+                        return "\(double)"
+                    } else if let float = value as? Float {
+                        return "\(float)"
+                    } else if let decimal = value as? Decimal {
+                        return "\(decimal)"
+                    } else if let int8 = value as? Int8 {
+                        return "\(int8)"
+                    } else if let int16 = value as? Int16 {
+                        return "\(int16)"
+                    } else if let int32 = value as? Int32 {
+                        return "\(int32)"
+                    } else if let int64 = value as? Int64 {
+                        return "\(int64)"
+                    } else if let int = value as? Int {
+                        return "\(int)"
+                    } else if let string = value as? String {
                         return "'\(string)'"
+                    } else if let uint8 = value as? UInt8 {
+                        return "\(uint8)"
+                    } else if let uint16 = value as? UInt16 {
+                        return "\(uint16)"
+                    } else if let uint32 = value as? UInt32 {
+                        return "\(uint32)"
+                    } else if let uint64 = value as? UInt64 {
+                        return "\(uint64)"
+                    } else if let uint = value as? UInt {
+                        return "\(uint)"
                     } else if let url = value as? URL {
                         return "'\(url.absoluteString)'"
+                    } else if let uuid = value as? UUID {
+                        return "'\(uuid.uuidString)'"
                     }
 
-                    return "\(value)"
+                    return "'\(value)'"
                 }
 
                 return "\(Kind.null)"
--- Sources/Tuproq/Expression/SQL/UpdateSQLExpression.swift
+++ Sources/Tuproq/Expression/SQL/UpdateSQLExpression.swift
@@ -4,7 +4,10 @@ final class UpdateSQLExpression: SQLExpression {
     let table: String
     let values: [(String, Any?)]
 
-    init(table: String, values: [(String, Any?)]) {
+    init(
+        table: String,
+        values: [(String, Any?)]
+    ) {
         self.table = table
         self.values = values
         var raw = "\(Kind.update) \(table)"
@@ -18,12 +21,48 @@ final class UpdateSQLExpression: SQLExpression {
                 if let value = value.1 {
                     if value as AnyObject is NSNull {
                         raw += " = NULL"
+                    } else if let bool = value as? Bool {
+                        raw += " = \(bool)"
+                    } else if let character = value as? Character {
+                        raw += " = '\(character)'"
+                    } else if let data = value as? Data {
+                        raw += " = '\(String(data: data, encoding: .utf8) ?? "")'"
+                    } else if let date = value as? Date {
+                        raw += " = '\(date)'"
+                    } else if let double = value as? Double {
+                        raw += " = \(double)"
+                    } else if let float = value as? Float {
+                        raw += " = \(float)"
+                    } else if let decimal = value as? Decimal {
+                        raw += " = \(decimal)"
+                    } else if let int8 = value as? Int8 {
+                        raw += " = \(int8)"
+                    } else if let int16 = value as? Int16 {
+                        raw += " = \(int16)"
+                    } else if let int32 = value as? Int32 {
+                        raw += " = \(int32)"
+                    } else if let int64 = value as? Int64 {
+                        raw += " = \(int64)"
+                    } else if let int = value as? Int {
+                        raw += " = \(int)"
                     } else if let string = value as? String {
                         raw += " = '\(string)'"
+                    } else if let uint8 = value as? UInt8 {
+                        raw += " = \(uint8)"
+                    } else if let uint16 = value as? UInt16 {
+                        raw += " = \(uint16)"
+                    } else if let uint32 = value as? UInt32 {
+                        raw += " = \(uint32)"
+                    } else if let uint64 = value as? UInt64 {
+                        raw += " = \(uint64)"
+                    } else if let uint = value as? UInt {
+                        raw += " = \(uint)"
                     } else if let url = value as? URL {
                         raw += " = '\(url.absoluteString)'"
+                    } else if let uuid = value as? UUID {
+                        raw += " = '\(uuid.uuidString)'"
                     } else {
-                        raw += " = \(value)"
+                        raw += " = '\(value)'"
                     }
                 } else {
                     raw += " = NULL"
@@ -37,3 +76,23 @@ final class UpdateSQLExpression: SQLExpression {
         super.init(raw: raw)
     }
 }
+
+//case bool
+//case character
+//case data(length: UInt? = nil, isFixed: Bool = false)
+//case date
+//case decimal(precision: UInt, scale: UInt, isUnsigned: Bool = false)
+//case double(isUnsigned: Bool = false)
+//case float(isUnsigned: Bool = false)
+//case id(_ strategy: IDGeneratorStrategy = .auto)
+//case int8
+//case int16
+//case int32
+//case int64
+//case string(length: UInt? = nil, isFixed: Bool = false)
+//case uint8
+//case uint16
+//case uint32
+//case uint64
+//case url
+//case uuid
