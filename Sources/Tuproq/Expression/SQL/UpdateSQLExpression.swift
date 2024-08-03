17414686818a6b43c78e3f1b65acff6fdb7abf92
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2024-08-03 18:02:14 +0500

    Remove commented code

--- Sources/Tuproq/Expression/SQL/UpdateSQLExpression.swift
+++ Sources/Tuproq/Expression/SQL/UpdateSQLExpression.swift
@@ -76,23 +76,3 @@ final class UpdateSQLExpression: SQLExpression {
         super.init(raw: raw)
     }
 }
-
-//case bool
-//case character
-//case data(length: UInt? = nil, isFixed: Bool = false)
-//case date
-//case decimal(precision: UInt, scale: UInt, isUnsigned: Bool = false)
-//case double(isUnsigned: Bool = false)
-//case float(isUnsigned: Bool = false)
-//case id(_ strategy: IDGeneratorStrategy = .auto)
-//case int8
-//case int16
-//case int32
-//case int64
-//case string(length: UInt? = nil, isFixed: Bool = false)
-//case uint8
-//case uint16
-//case uint32
-//case uint64
-//case url
-//case uuid
