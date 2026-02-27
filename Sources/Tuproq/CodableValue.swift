ab4ad68943064f95fd1fadcc6a983be2430f9249
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2026-02-27 11:39:33 +0500

    Fix decoding/encoding sequence for CodableValue

--- Sources/Tuproq/CodableValue.swift
+++ Sources/Tuproq/CodableValue.swift
@@ -16,64 +16,165 @@ public enum CodableValue: Codable, Hashable, Sendable {
     case null
 
     public init(from decoder: Decoder) throws {
+        if let container = try? decoder.container(keyedBy: DynamicCodingKey.self) {
+            var dict: [String: Self] = [:]
+            for key in container.allKeys {
+                dict[key.stringValue] =
+                    try container.decode(Self.self, forKey: key)
+            }
+            self = .dictionary(dict)
+            return
+        }
+
+        if var container = try? decoder.unkeyedContainer() {
+            var array: [Self] = []
+            while !container.isAtEnd {
+                array.append(try container.decode(Self.self))
+            }
+            self = .array(array)
+            return
+        }
+
         let container = try decoder.singleValueContainer()
-        guard !container.decodeNil() else {
+
+        if container.decodeNil() {
             self = .null
             return
         }
 
         if let bool = try? container.decode(Bool.self) {
             self = .bool(bool)
-        } else if let int = try? container.decode(Int64.self) {
-            self = .int(int)
-        } else if let uint = try? container.decode(UInt64.self) {
-            self = .uint(uint)
-        } else if let double = try? container.decode(Double.self) {
-            self = .double(double)
-        } else if let string = try? container.decode(String.self) {
-            self = .string(string)
-        } else if let decimal = try? container.decode(Decimal.self) {
-            self = .decimal(decimal)
-        } else if let date = try? container.decode(Date.self) {
-            self = .date(date)
-        } else if let uuid = try? container.decode(UUID.self) {
-            self = .uuid(uuid)
-        } else if let data = try? container.decode(Data.self) {
-            self = .data(data)
-        } else if let url = try? container.decode(URL.self) {
-            self = .url(url)
-        } else if let dictionary = try? container.decode([String: Self].self) {
-            self = .dictionary(dictionary)
-        } else if let array = try? container.decode([Self].self) {
-            self = .array(array)
-        } else {
-            throw DecodingError.typeMismatch(
-                Self.self,
-                .init(
-                    codingPath: decoder.codingPath,
-                    debugDescription: "Unsupported Codable type"
-                )
-            )
+            return
         }
+
+        if let int = try? container.decode(Int64.self) {
+            self = .int(int)
+            return
+        }
+
+        if let uint = try? container.decode(UInt64.self) {
+            self = .uint(uint)
+            return
+        }
+
+        if let double = try? container.decode(Double.self) {
+            self = .double(double)
+            return
+        }
+
+        if let decimal = try? container.decode(Decimal.self) {
+            self = .decimal(decimal)
+            return
+        }
+
+        if let date = try? container.decode(Date.self) {
+            self = .date(date)
+            return
+        }
+
+        if let uuid = try? container.decode(UUID.self) {
+            self = .uuid(uuid)
+            return
+        }
+
+        if let data = try? container.decode(Data.self) {
+            self = .data(data)
+            return
+        }
+
+        if let url = try? container.decode(URL.self) {
+            self = .url(url)
+            return
+        }
+
+        if let string = try? container.decode(String.self) {
+            self = .string(string)
+            return
+        }
+
+        throw DecodingError.typeMismatch(
+            Self.self,
+            .init(
+                codingPath: decoder.codingPath,
+                debugDescription: "Unsupported Codable type"
+            )
+        )
     }
 
     public func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
 
         switch self {
-        case .string(let string): try container.encode(string)
-        case .bool(let bool): try container.encode(bool)
-        case .int(let int): try container.encode(int)
-        case .uint(let uint): try container.encode(uint)
-        case .double(let double): try container.encode(double)
-        case .decimal(let decimal): try container.encode(decimal)
-        case .date(let date): try container.encode(date)
-        case .uuid(let uuid): try container.encode(uuid)
-        case .data(let data): try container.encode(data)
-        case .url(let url): try container.encode(url)
-        case .array(let array): try container.encode(array)
-        case .dictionary(let dictionary): try container.encode(dictionary)
-        case .null: try container.encodeNil()
+
+        case .dictionary(let dict):
+            var container = encoder.container(keyedBy: DynamicCodingKey.self)
+            for (key, value) in dict {
+                try container.encode(value, forKey: .init(stringValue: key))
+            }
+
+        case .array(let array):
+            var container = encoder.unkeyedContainer()
+            for value in array {
+                try container.encode(value)
+            }
+
+        case .string(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .bool(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .int(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .uint(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .double(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .decimal(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .date(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .uuid(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .data(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .url(let v):
+            var container = encoder.singleValueContainer()
+            try container.encode(v)
+
+        case .null:
+            var container = encoder.singleValueContainer()
+            try container.encodeNil()
         }
+    }
+}
+
+private struct DynamicCodingKey: CodingKey {
+    var stringValue: String
+    var intValue: Int?
+
+    init(stringValue: String) {
+        self.stringValue = stringValue
+        self.intValue = nil
+    }
+
+    init(intValue: Int) {
+        self.stringValue = "\(intValue)"
+        self.intValue = intValue
     }
 }
